ecb335b952141934fd464b095d2ad996e95b61e3
Diab Neiroukh <officiallazerl0rd@gmail.com>
2020-09-24 12:02:17 -0700

    Create Faster Animations.bat

---
+++ Faster Animations.bat
@@ -0,0 +1,18 @@
+title "ScriptWINg: Faster Animations"
+cls
+
+net session >nul 2>nul
+net session >nul 2>nul
+if not "%errorlevel%" == "0" (
+	echo "You must run the script as an administrator."
+	pause
+	exit /b 1
+)
+
+echo "    - Beginning registry changes"
+1>NUL reg add "HKEY_CURRENT_USER\Control Panel\Desktop" /v "MenuShowDelay" /t REG_DWORD /d 100 /f
+
+cls
+echo "Faster Animations has finished running."
+pause
+exit /b 0
